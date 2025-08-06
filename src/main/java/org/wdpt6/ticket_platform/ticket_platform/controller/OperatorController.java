d20772661b034af3ceab0effa20c0e8b56e98862
emanuelespina <spina.emanuele98@gmail.com>
2025-08-06 08:07:36 +0200

    fead: try change user avaible

--- src/main/java/org/wdpt6/ticket_platform/ticket_platform/controller/OperatorController.java
+++ src/main/java/org/wdpt6/ticket_platform/ticket_platform/controller/OperatorController.java
@@ -1,6 +1,7 @@
 package org.wdpt6.ticket_platform.ticket_platform.controller;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -23,6 +24,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -129,6 +132,48 @@ public class OperatorController {
 
         return "redirect:/operators/changer/" + ticket.getId();
     }
+
+    @GetMapping("/profile/{id}")
+    public String getMethodName(@PathVariable Integer id, Model model) {
+      
+        model.addAttribute("user", userRepository.findById(id).get());     
+
+        return "operators/profile";
+    }
+
+    @PostMapping("/profile/{id}")
+    public String postMethodName(@PathVariable Integer id, @RequestParam boolean setAnvaible) {
+        
+        User user = userRepository.findById(id).get();
+
+        List<Ticket> tickets = ticketRepository.findByUser(user);
+
+        boolean inCorso = false;
+
+        for (Ticket ticket : tickets) {
+
+            if (ticket.getStatus().equals(TicketStatus.valueOf("IN_CORSO"))) {
+
+                inCorso = true;
+                
+            }
+            
+        }
+
+        if (inCorso) {
+
+            user.setNotAvailable(false);
+            
+        }else {
+
+            user.setNotAvailable(setAnvaible);
+
+        }
+        
+        return "redirect:/operators/changer/" + user.getId();
+    }
+    
+    
 
 
 
